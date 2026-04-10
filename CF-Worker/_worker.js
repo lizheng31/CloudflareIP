232e8ebf7f00250c2995dce3867d13f02f65fabe
ronron <lizheng31@qq.com>
2026-04-10 09:31:48 +0800

    Update _worker.js
    use my uuid

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = '65de26a4-dbda-4924-9520-3dfdc93b3f57';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
